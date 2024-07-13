8586d307fef2c921e1de898e0a2d6e59c27202e6
Sophia <lsofa1204@gmail.com>
2024-07-14 00:14:59 +0300

    fix footer

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,14 @@ import { Main } from "@/components/Main";
 import Image from "next/image";
 import "../styles/reset.css"
 import "../styles/styles.css"
+import { Footer } from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Main />
+      <Footer/>
     </>
   );
 }
